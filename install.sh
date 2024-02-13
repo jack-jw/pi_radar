9d007af76fdfa33b1a6f99f339aaa96696d1d3a2
Jack Woodward <131390418+yellowcress@users.noreply.github.com>
2024-02-13 12:42:28 +0000

    work on install.sh

--- install.sh
+++ install.sh
@@ -4,22 +4,35 @@ read -p "Install Pi-radar? [y/N] " response
 response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
 
 if [[ "$response" =~ ^(yes|y)$ ]]; then
-	echo "Running sudo -v (may require password)"
+	echo "Running sudo -v (may require password)..."
 	sudo -v
-	cd /var/lib/
-	echo "Cloning github.com/yellowcress/pi-radar..."
-    sudo git clone https://github.com/yellowcress/pi-radar.git
-    cd /var/lib/pi-radar
-    sudo rm /.git
+    cd /tmp
     
-    echo "Installing dependencies"
-    sudo apt install -y python3-requests python3-bs4 python3-pandas
+    echo "Installing pip..."
+    curl -Os https://bootstrap.pypa.io/get-pip.py
+    sudo python3 get-pip.py
+	
+    echo "Installing pip-managed dependencies (pyrtlsdr, requests, bs4, pandas)..."
+    sudo pip install -q pyrtlsdr requests bs4 pandas
     
-    # more here
+    echo "Installing apt-managed dependency (librtlsdr0)..."
+    sudo apt install -qqy librtlsdr0
+	
+	# more here
+	
+	echo "Installing Pi-radar"
+    curl -Os https://codeload.github.com/yellowcress/[URL HERE]/zip/refs/heads/main
+    sudo unzip main -d /var/lib/pi-radar
+
+    # more here maybe too
     
     echo "Adding and enabling service..."
     sudo cp /var/lib/pi-radar/pi-radar.service /etc/systemd/system/
     sudo systemctl enable pi-radar.service
+    
+    echo "Cleaning up..."
+    sudo rm /tmp/get-pip.py /tmp/main /
+    
     echo "Starting pi-radar..."
     sudo systemctl start pi-radar.service
 else
