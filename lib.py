f204f27b1d4be3bed816fc6060f28678aef054ab
Jack Woodward <131390418+yellowcress@users.noreply.github.com>
2024-02-17 13:17:59 +0000

    I can’t decide what to call the module

--- lib.py
+++ lib.py
@@ -1,10 +1,11 @@
 import sqlite3
 
+database = "main.db"
 airframeHeaders = ('ICAO24', 'Registration', 'Manufacturer ICAO', 'Manufacturer name', 'Model', 'Type code', 'Serial number', 'Line number', 'ICAO type', 'Operator', 'Operator callsign', 'Operator ICAO', 'Operator IATA', 'Owner', 'Test registration', 'Registered', 'Registered until', 'Status', 'Built', 'First flight', 'Seat configuration', 'Engines', 'Modes', 'ADSB', 'ACARS', 'Notes', 'Category description')
 
 def airframe(address):
 	address = address.lower()
-	mainDB = sqlite3.connect('main.db')
+	mainDB = sqlite3.connect(database)
 	cursor = mainDB.cursor()
 	cursor.execute(f"SELECT * FROM airframes WHERE icao24 = '{address}'")
 	airframeInfo = cursor.fetchone()
@@ -18,7 +19,7 @@ def airframe(address):
 	
 def airportICAO(iata):
 	iata = iata.upper()
-	mainDB = sqlite3.connect('main.db')
+	mainDB = sqlite3.connect(database)
 	cursor = mainDB.cursor()
 	cursor.execute(f"SELECT gps_code FROM airports WHERE iata_code = '{iata}'")
 	result = cursor.fetchone()
@@ -28,7 +29,7 @@ def airportICAO(iata):
 	return result
 
 def airportName(code):
-	mainDB = sqlite3.connect('main.db')
+	mainDB = sqlite3.connect(database)
 	cursor = mainDB.cursor()
 	
 	if len(code) == 4:
@@ -43,29 +44,32 @@ def airportName(code):
 	result = result[0]
 	return result
 
-def callsign(callsign):
+def airline(callsign):
 	callsign = callsign.upper()
-	callsign = callsign[:3]
-	mainDB = sqlite3.connect('main.db')
+	code = callsign[:3]
+	mainDB = sqlite3.connect(database)
 	cursor = mainDB.cursor()
-	cursor.execute(f"SELECT Airline FROM callsigns WHERE ICAO = '{callsign}'")
+	cursor.execute(f"SELECT Airline FROM callsigns WHERE ICAO = '{code}'")
 	result = cursor.fetchone()
 	cursor.close()
 	mainDB.close()
 	result = result[0]
 	return result
 
-def route(route):
-	route = route.upper()
-	callsign = route[:3]
-	routesTable = "routes" + callsign
-	mainDB = sqlite3.connect('main.db')
+def route(callsign):
+	callsign = callsign.upper()
+	code = callsign[:3]
+	routesTable = "routes" + code
+	
+	mainDB = sqlite3.connect(database)
 	cursor = mainDB.cursor()
+	
 	cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name = '{routesTable}'")
-	if (cursor.fetchone() is not None) and callsign != "BAW":
-		cursor.execute(f"SELECT Route FROM {routesTable} WHERE Callsign = '{route}'")
-	else:
-		cursor.execute(f"SELECT `1000-1299(Other)` FROM routesBAW WHERE `1301-1499(Domestic)` LIKE '%{route}%'")
+	if (cursor.fetchone() is None):
+		routesTable = "routesBAW"
+	
+	cursor.execute(f"SELECT Route FROM {routesTable} WHERE Callsign LIKE '%{callsign}%'")
+	
 	result = cursor.fetchone()
 	cursor.close()
 	mainDB.close()
@@ -76,15 +80,30 @@ def convertDBs():
 	from pandas import read_csv
 	import glob
 	import os
-	mainDB = sqlite3.connect("main.db")
+	mainDB = sqlite3.connect(database)
 	
 	csvs = glob.glob("*.csv")
 	for database in csvs:
 		db = read_csv(database)
 		tableName = database.replace(".csv","")
 		db.to_sql(tableName, mainDB, index=False, if_exists='replace')
+		mainDB.commit()
 		os.remove(database)
 	
+	cursor = mainDB.cursor()
+	
+	try:
+		cursor.execute('DROP TABLE routesBAW')
+		mainDB.commit()
+	except:
+		pass
+	
+	cursor.execute('''
+	CREATE TABLE routesBAW AS
+	SELECT "1301-1499(Domestic)" AS Callsign,  "1000-1299(Other)" AS Route
+	FROM routesBAWRaw''')
+	mainDB.commit()
+	cursor.execute('DROP TABLE routesBAWRaw')
 	mainDB.commit()
 	mainDB.close()
 	
@@ -103,29 +122,29 @@ def updateDBs():
 	with open('airports.csv', 'w', newline='', encoding='utf-8') as csvFile:
 	    csvFile.write(response.text)
 	
-	response = requests.get("https://speedbird.online/flightnumbers.php")
-	soup = BeautifulSoup(response.text, 'html.parser')
-	tables = soup.find_all('table')
-	if len(tables) >= 3:
-		table = tables[2]
-		rows = table.find_all('tr')
-		with open('routesBAW.csv', 'w', newline='', encoding='utf-8') as csvFile:
-			csvWriter = csv.writer(csvFile)
-			for row in rows[1:]:
-				data = [cell.text.strip() for cell in row.find_all('td')]
-				csvWriter.writerow(data)
-	
 	response = requests.get("https://en.wikipedia.org/wiki/List_of_airline_codes")
 	match = re.search('<table[^>]*>(.*?)</table>', response.text, re.DOTALL)
 	if match:
 		tableContent = match.group(1)
 	soup = BeautifulSoup(tableContent, 'html.parser')
-	with open('callsigns.csv', 'w', newline='', encoding='utf-8') as csvFile:
+	with open('codes.csv', 'w', newline='', encoding='utf-8') as csvFile:
 		csvWriter = csv.writer(csvFile)
 		rows = soup.find_all('tr')
 		for row in rows:
 			cells = row.find_all(['th', 'td'])
 			data = [cell.get_text(strip=True) for cell in cells]
 			csvWriter.writerow(data)
+			
+	response = requests.get("https://speedbird.online/flightnumbers.php")
+	soup = BeautifulSoup(response.text, 'html.parser')
+	tables = soup.find_all('table')
+	if len(tables) >= 3:
+		table = tables[2]
+		rows = table.find_all('tr')
+		with open('routesBAWRaw.csv', 'w', newline='', encoding='utf-8') as csvFile:
+			csvWriter = csv.writer(csvFile)
+			for row in rows[1:]:
+				data = [cell.text.strip() for cell in row.find_all('td')]
+				csvWriter.writerow(data)
 	        
 	convertDBs()
