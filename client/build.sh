d5631ebb6c95546737724aad24c1780d0a711ced
Jack Woodward <131390418+yellowcress@users.noreply.github.com>
2024-02-17 22:36:49 +0000

    zip client (probably won’t use but just in case)

--- client/build.sh
+++
@@ -1,6 +0,0 @@
-scriptDir=$(dirname "$0")
-
-cd "$scriptDir"
-echo "Compiling $scriptDir/client.applescript to $scriptDir/Pi-radar.app/Contents/Resources/Scripts/main.scpt"
-osacompile -x -o "$scriptDir/Pi-radar.app/Contents/Resources/Scripts/main.scpt" "$scriptDir/client.applescript"
-open -a "$scriptDir/Pi-radar.app"
