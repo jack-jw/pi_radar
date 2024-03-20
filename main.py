ea795f170a6e6682511ec02b78719f4d3a8a22c1
Jack Woodward <131390418+yellowcress@users.noreply.github.com>
2024-03-20 23:21:28 +0000

    move server.py to main.py

--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 # Piradar
-# main.py
+# server.py
 
 """
-Basic lookup interface for now - delete later
-Better to test functions through the lookup module.
+Control the HTTP server
+
+Functions:
+    start()
 """
 
-from backend import lookup
-import server
+from os import urandom
+from threading import Thread
+from flask import Flask, render_template
+from flask_socketio import SocketIO#, emit
+import backend
 
-def printdict(dictionary):
-    """
-    Prints the passed dictionary nicely.
-    """
+def start():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = urandom(24)
+    socketio = SocketIO(app)
 
-    if dictionary is not None:
-        for header, value in dictionary.items():
-            print(f"{header}: {value}")
-    else:
-        print(None)
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-server.start()
+    @app.route('/map')
+    def map():
+        return render_template('map.html')
 
-menu = input("Search for a airline, a route, an aircraft or an airport? [l/r/c/p] ")
-if menu == "l":
-    printdict(lookup.airline(input("Callsign: ")))
-elif menu == "r":
-    printdict(lookup.route(input("Route: ")))
-elif menu == "c":
-    printdict(lookup.aircraft(input("ICAO24 address: ")))
-elif menu == "p":
-    printdict(lookup.airport(input("IATA code: ")))
-else:
-    print("Invalid option")
+    @socketio.on('connect')
+    def handle_connect():
+        print('Client connected')
+
+    @socketio.on('info')
+    def handle_aircraft_info_query(address):
+        emit('info', backend.lookup.aircraft(address))
+
+    @socketio.on('disconnect')
+    def handle_disconnect():
+        print('Client disconnected')
+
+    socket_thread = Thread(target=socketio.run, args=(app, 'localhost', 5001))
+    #socket_thread.daemon = True
+    socket_thread.start()
+
+if __name__ == "__main__":
+    start()
